7bb5414651d1c7d484b56dd65c5cb4553ebca35e
ydw1996 <112791174+ydw1996@users.noreply.github.com>
2022-09-15 17:21:21 +0900

    init

---
+++ js/index.js
@@ -0,0 +1,124 @@
+var mHtml = $("html");
+var page = 1;
+
+$(document).ready(function () {
+  $(".header__menu ul").fadeIn(1500);
+});
+
+$(document).ready(function () {
+  var jbTime = 80;
+  $(".st07").animate(
+    {
+      opacity: "1",
+    },
+    jbTime,
+    function () {
+      $(".st06").animate(
+        {
+          opacity: "1",
+        },
+        jbTime,
+        function () {
+          $(".st05").animate(
+            {
+              opacity: "1",
+            },
+            jbTime,
+            function () {
+              $(".st04").animate(
+                {
+                  opacity: "1",
+                },
+                jbTime,
+                function () {
+                  $(".st03").animate(
+                    {
+                      opacity: "1",
+                    },
+                    jbTime,
+                    function () {
+                      $(".st02").animate(
+                        {
+                          opacity: "1",
+                        },
+                        jbTime,
+                        function () {
+                          $(".st01").animate(
+                            {
+                              opacity: "1",
+                            },
+                            jbTime
+                          );
+                        }
+                      );
+                    }
+                  );
+                }
+              );
+            }
+          );
+        }
+      );
+    }
+  );
+});
+
+
+
+$(document).ready(function () {
+  var jbTime = 200;
+  $(".st07").animate(
+    {
+      opacity: "0",
+    },
+    jbTime,
+    function () {
+      $(".st06").animate(
+        {
+          opacity: "0",
+        },
+        jbTime,
+        function () {
+          $(".st05").animate(
+            {
+              opacity: "0",
+            },
+            jbTime,
+            function () {
+              $(".st04").animate(
+                {
+                  opacity: "0",
+                },
+                jbTime,
+                function () {
+                  $(".st03").animate(
+                    {
+                      opacity: "0",
+                    },
+                    jbTime,
+                    function () {
+                      $(".st02").animate(
+                        {
+                          opacity: "0",
+                        },
+                        jbTime,
+                        function () {
+                          $(".st01").animate(
+                            {
+                              opacity: "1",
+                            },
+                            jbTime
+                          );
+                        }
+                      );
+                    }
+                  );
+                }
+              );
+            }
+          );
+        }
+      );
+    }
+  );
+});
