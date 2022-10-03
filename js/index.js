fc4df6e2ed72dabc570c364faea499ac3274f65c
ydw1996 <112791174+ydw1996@users.noreply.github.com>
2022-10-03 23:41:11 +0900

    tototo

--- js/index.js
+++ js/index.js
@@ -3,6 +3,48 @@ var page = 1;
 
 $(document).ready(function () {
   $(".header__menu ul").fadeIn(1500);
+
+  $(document).scroll(() => {
+    if (window.scrollY >= 1700) {
+      if (!$(".sec02_con .to01").hasClass("op")) {
+        $(".sec02_con .to01").addClass("tototo01");
+        setTimeout(() => {
+          $(".sec02_con .to01").removeClass("tototo01");
+          $(".sec02_con .to01").addClass("op");
+        }, 100);
+        setTimeout(() => {
+          $(".sec02_con .to02").addClass("tototo02");
+        }, 100);
+        setTimeout(() => {
+          $(".sec02_con .to02").removeClass("tototo02");
+          $(".sec02_con .to02").addClass("op");
+        }, 250);
+        setTimeout(() => {
+          $(".sec02_con .to03").addClass("tototo03");
+        }, 200);
+        setTimeout(() => {
+          $(".sec02_con .to03").removeClass("tototo03");
+          $(".sec02_con .to03").addClass("op");
+        }, 350);
+        setTimeout(() => {
+          $(".sec02_con .to04").addClass("tototo04");
+        }, 300);
+        setTimeout(() => {
+          $(".sec02_con .to04").removeClass("tototo04");
+          $(".sec02_con .to04").addClass("op");
+        }, 450);
+        setTimeout(() => {
+          $(".sec02_con .to05").addClass("tototo05");
+        }, 400);
+        setTimeout(() => {
+          $(".sec02_con .to05").removeClass("tototo05");
+          $(".sec02_con .to05").addClass("op");
+        }, 550);
+      }
+    } else if (window.scrollY <= 1200) {
+      $(".sec02_con em").removeClass("op");
+    }
+  });
 });
 /* $(document).ready(function () {
   $(".sub01__subtitle").fadeIn(1500);
